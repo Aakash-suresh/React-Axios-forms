a26cfcc1f7025036616e5b9fce834d322395ab86
Aakash Suresh <aakashakshith455@gmail.com>
2023-04-11 11:32:20 +0530

    Final commit- Finished one

--- src/Components/calculatorComponent/calculatorComponent.jsx
+++ src/Components/calculatorComponent/calculatorComponent.jsx
@@ -1,73 +1,93 @@
 import React, { Component } from 'react';
-// import 'react-exp-1/src/Components/calculatorComponent/calculatorComponent.css';
 import '../calculatorComponent/calculatorComponent.css'
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 export class CalculatorComponent extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-          num1: 0,
-          num2: 0,
-          result: 0
-        };
-      }
-    
-      handleNum1Change = (event) => {
-        this.setState({ num1: parseInt(event.target.value) });
-      }
-    
-      handleNum2Change = (event) => {
-        this.setState({ num2: parseInt(event.target.value) });
-      }
-    
-      handleAdd = () => {
-        this.setState({ result: this.state.num1 + this.state.num2 });
-      }
-    
-      handleSubtract = () => {
-        this.setState({ result: this.state.num1 - this.state.num2 });
-      }
-    
-      handleMultiply = () => {
-        this.setState({ result: this.state.num1 * this.state.num2 });
-      }
-    
-      handleDivide = () => {
-        this.setState({ result: this.state.num1 / this.state.num2 });
-      }
-    
-      render() {
-        return (
-          <div className="calculator">
-            <h2>Calculator</h2>
-            <form>
-              <div className="number-inputs">
-                <label>
-                  Number 1:
-                  <input type="number" value={this.state.num1} onChange={this.handleNum1Change} />
-                </label>
-                <label>
-                  Number 2:
-                  <input type="number" value={this.state.num2} onChange={this.handleNum2Change} />
-                </label>
-              </div>
-              <div className="operator-buttons">
-                <button type="button" onClick={this.handleAdd}>+</button>
-                <button type="button" onClick={this.handleSubtract}>-</button>
-                <button type="button" onClick={this.handleMultiply}>*</button>
-                <button type="button" onClick={this.handleDivide}>/</button>
-              </div>
-              <div className="result-input">
-                <label>
-                  Result:
-                  <input type="number" value={this.state.result} disabled />
-                </label>
-              </div>
-            </form>
+  constructor(props) {
+    super(props);
+    this.state = {
+      num1: 0,
+      num2: 0,
+      result: 0
+    };
+  }
+
+  handleNum1Change = (event) => {
+    this.setState({ num1: parseInt(event.target.value) });
+  }
+
+  handleNum2Change = (event) => {
+    this.setState({ num2: parseInt(event.target.value) });
+  }
+
+  handleAdd = () => {
+    if (this.state.num1 === 0 && this.state.num2 === 0) {
+      alert("Please enter valid input");
+    } else {
+      this.setState({ result: this.state.num1 + this.state.num2 });
+    }
+  }
+
+  handleSubtract = () => {
+    if (this.state.num1 === 0 && this.state.num2 === 0) {
+      alert("Please enter valid input");
+    } else {
+      this.setState({ result: this.state.num1 - this.state.num2 });
+    }
+  }
+
+  handleMultiply = () => {
+    if (this.state.num1 === 0 && this.state.num2 === 0) {
+      alert("Please enter valid input");
+    } else {
+      this.setState({ result: this.state.num1 * this.state.num2 });
+    }
+  }
+
+  handleDivide = () => {
+    if (this.state.num1 === 0 && this.state.num2 === 0) {
+      alert("Please enter valid input");
+    } else {
+      this.setState({ result: this.state.num1 / this.state.num2 });
+    }
+  }
+
+  handleClear = () => {
+    this.setState({ num1: 0, num2: 0, result: 0 });
+  }
+
+  render() {
+    return (
+      <div className="calculator">
+        <h2>Calculator</h2>
+        <form>
+          <div className="number-inputs">
+            <label>
+              Number 1:
+              <input type="text" value={this.state.num1} onChange={this.handleNum1Change} />
+            </label>
+            <label>
+              Number 2:
+              <input type="text" value={this.state.num2} onChange={this.handleNum2Change} />
+            </label>
           </div>
-        );
-      }
+          <div className="operator-buttons">
+            <button type="button" onClick={this.handleAdd}>+</button>
+            <button type="button" onClick={this.handleSubtract}>-</button>
+            <button type="button" onClick={this.handleMultiply}>*</button>
+            <button type="button" onClick={this.handleDivide}>/</button>
+            <button type="button" onClick={this.handleClear}>Clear</button>
+          </div>
+          <div className="result-input">
+            <label>
+              Result:
+              <input type="number" value={this.state.result} disabled />
+            </label>
+          </div>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default CalculatorComponent;
